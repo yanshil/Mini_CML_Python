b2705fe4baf39a9d7e361c30d5e9f1826fb3971b
Yanshi Luo <yanshi.luo@rutgers.edu>
2021-06-25 08:23:16 +0000

    with -e . working

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## Mini CML python
 
 This is a simple example python CML Tools package.
+
+Example command line: `hello`
+
+```
+$hello
+Hello!
+```
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
-name = 'pyserializer_kdrobian'
+name = 'Mini_CML_example_kdrobian'
 version = '0.1'
 dependencies = []
 
@@ -17,7 +17,6 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=dependencies,
-    include_package_data=True,
     entry_points={
         'console_scripts': [
             'hello = src.hello_world:main',
